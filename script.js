313202f8f66487b7f104bb4d2e4206c05edcce96
maickelraj S <maickelraj052005@gmail.com>
2026-01-01 19:29:50 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,22 +1,17 @@
 let quantity = 1;
 
-const minusBtn = document.querySelector(".quantity-box button:first-child");
-const plusBtn = document.querySelector(".quantity-box button:last-child");
-const quantityText = document.querySelector(".quantity-box span");
-const addToCartBtn = document.querySelector(".add-to-cart");
-
-plusBtn.addEventListener("click", () => {
+function increaseQty() {
   quantity++;
-  quantityText.textContent = quantity;
-});
+  document.getElementById("qty").innerText = quantity;
+}
 
-minusBtn.addEventListener("click", () => {
+function decreaseQty() {
   if (quantity > 1) {
     quantity--;
-    quantityText.textContent = quantity;
+    document.getElementById("qty").innerText = quantity;
   }
-});
+}
 
-addToCartBtn.addEventListener("click", () => {
-  alert(`Added ${quantity} item(s) to cart`);
-});
+function addToCart() {
+  alert("Plum Brownie Cake added to cart! Quantity: " + quantity);
+}
